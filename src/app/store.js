76f2c3d16c088df83ef049902fa95a18544cd81a
Emmanuel Donkor <edonkor0000@gmail.com>
2024-02-11 16:54:00 +0100

    prod: removed statistics slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import expensesSlice from './expensesSlice';
-import statisticsSlice from './statisticsSlice';
 import ToastMiddleWare from '../middlewares/ToastMiddleWare';
 import authenticationSlice from './authenticationSlice';
 export default configureStore({
   reducer: {
     authenticationSlice: authenticationSlice,
     expensesSlice: expensesSlice,
-    statisticsSlice: statisticsSlice
+  
   },
   middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(ToastMiddleWare)
 });
